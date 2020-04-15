afc468399cc8c5b332e216482a20d93d724024d4
buscaroli <m.buscaroli@outlook.com>
2020-04-15 23:27:55 +0100

    weather-app: implemented a different pattern for errors (returning the error message instead of using an if else clause)

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,15 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
+/*********************************************************************
+ * In case we come across an error in the first callback (geocode)
+ * we don't want to proceed and try to display the (undefined) data
+ * inside the forecast callback.
+ * 
+ * In this case we have done this by simply enclosing the second
+ * callback within an 'else' statement, so it will only get executed
+ * if there is no error.
+ * 
 
 geocode.geocode(forecast.town, (error, data) => {
     if (error) {
@@ -19,6 +28,44 @@ geocode.geocode(forecast.town, (error, data) => {
             
         });
     }
+    
+});
+
+******************************************************************************* */
+
+
+/*********************************************************************
+ * In case we come across an error in the first callback (geocode)
+ * we don't want to proceed and try to display the (undefined) data
+ * inside the forecast callback.
+ * 
+ * In this case we have use a different but common pattern that is
+ * worth bearing in mind:
+ * In case of an error we 'return' the console.log call.
+ * In fact the return statement  can be called only one in any given
+ * function, if it's called in the case of an error we are sure that
+ * the rest of the function won't be called.
+ * 
+ ******************************************************************* */
+
+geocode.geocode(forecast.town, (error, data) => {
+    if (error) {
+        return console.log('Error: ' + error);
+    } 
+    forecast(data.latitude, data.longitude, (error, data) => {
+
+        if (error) {
+            return console.log('Error: ' + error);
+        } 
+        
+        console.log('Location: ' + data.location + ', ' + data.country + '.')
+        console.log('Temperature: ' + data.temperature + ' C.');
+        console.log('Wind Speed: ', + data.wind_speed + ' Km/h');
+        console.log('UV Index: ' + data.uv_index);
+        
+        
+    });
+
     
 });
 
