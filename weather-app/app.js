79b45a9ddfaf5e5f0c07c61f23803b17f53e1666
buscaroli <m.buscaroli@outlook.com>
2020-04-16 20:55:46 +0100

    weather-forecast: amended package.json and added some comments to ap.js

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,3 +1,22 @@
+/**********************************************************************************
+ * LEARNING NODEJS
+ * Based on a Udemy video from Andrew Mead
+ * 
+ * Using a geolocation service's API (mapbox) and a weather forecast service's
+ * API (weatherstack) to get some information on a user specified location.
+ * 
+ * USE: node app.js 'location name'
+ * 
+ * TODO: At the moment weatherstack takes a long time to respond and sometimes
+ *      responds with an error object. Same happens if submitting the request
+ *      through the browser which means it's not a problem of the script.
+ *      Maybe I'll look into a different forecast provider if not solved in the
+ *      near future (currently April 2020).
+ *
+ * Matteo
+ * 
+ *********************************************************************************/
+
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
